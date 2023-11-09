4d2540966f1c9e99da06bfed3f56d7c5daf09060
Razib Hossain <Razib.Hossain@surbanajurong.com>
2023-11-09 14:33:39 +0800

    una added

---
+++ MileStoneProdEvents/Una/GenerateOutputExcel.cs
@@ -0,0 +1,68 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.IO.RecyclableMemoryStreamManager;
+
+namespace MileStoneProdEvents.Una
+{
+    public class GenerateOutputExcel
+    {
+        public void GenerateOutput(Dictionary<string, List<KeyValuePair<string, dynamic>>> dataMap)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+            using (var package = new ExcelPackage())
+            {
+                foreach (KeyValuePair<string, List<KeyValuePair<string, dynamic>>> kvp in dataMap)
+                {
+                    string deviceType = kvp.Key;
+                    List<KeyValuePair<string, dynamic>> values = kvp.Value;
+
+                    var worksheet = package.Workbook.Worksheets.Add(deviceType);
+                    PopulateWorksheet(worksheet, values);
+                }
+
+                // Save the Excel file
+                var customPath = @"C:\Users\LENOVO\Downloads\dummy\OutputFile.xlsx";
+                var excelFile = new FileInfo(customPath);
+                package.SaveAs(excelFile);
+            }
+        }
+
+        static void PopulateWorksheet(ExcelWorksheet worksheet, List<KeyValuePair<string, dynamic>> values)
+        {
+            int row = 1;
+            int col = 1;
+
+            //set headers
+            worksheet.Cells[row, col].Value = "FileName";
+            col++;
+            worksheet.Cells[row, col].Value = "Data";
+
+            values = values.OrderBy(kvp => UnixTimeStampToDateTime((long)kvp.Value.timestamp)).ToList();
+
+            //values = values.OrderBy(kvp => (long)kvp.Value.timestamp).ToList();
+
+            // Populate Excel cells with data from the object
+            foreach (var item in values)
+            {
+                row++;
+                col = 1;
+                worksheet.Cells[row, col++].Value = item.Key;
+                worksheet.Cells[row, col].Value = item.Value.ToString();
+
+            }
+        }
+
+        static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds((long)unixTimeStamp).UtcDateTime;
+        }
+
+
+    }
+}
+
---
+++ MileStoneProdEvents/Una/UnaStartup.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MileStoneProdEvents.Una
+{
+    public class UnaStartup
+    {
+        public void Start()
+        {
+
+            Dictionary<string, List<KeyValuePair<string, dynamic>>> dataMap = new Dictionary<string, List<KeyValuePair<string, dynamic>>>();
+
+            string sourceFolder = "C:\\Users\\LENOVO\\Downloads\\DataSourceUna";
+            string[] subdirectories = Directory.GetDirectories(sourceFolder);
+
+            foreach (string subdir in subdirectories)
+            {
+                string[] arr = Directory.GetFiles(subdir, "*.json");
+                string subDirectoryName = System.IO.Path.GetFileName(subdir);
+
+                foreach (string file in arr)
+                {
+                    string fileName = System.IO.Path.GetFileName(file);
+
+                    JObject jsonData = JObject.Parse(File.ReadAllText(file));
+                    var data = jsonData.ToObject<dynamic>();
+
+                    var deviceType = data.deviceType.ToString();
+
+                    if (deviceType != null)
+                    {
+                        if (dataMap.ContainsKey(deviceType))
+                        {
+                            dataMap[deviceType].Add(new KeyValuePair<string, dynamic>(subDirectoryName + "/" + fileName, data));
+                        }
+                        else
+                        {
+                            var values = new List<KeyValuePair<string, dynamic>>();
+                            values.Add(new KeyValuePair<string, dynamic>(subDirectoryName + "/" + fileName, data));
+
+                            dataMap.Add(deviceType, values);
+                        }
+                    }
+
+                    if (deviceType == "unabellV1M2" || deviceType == "unareadergenerator1.0.0" || deviceType == "detectifytp1.0.0")
+                    {
+
+                    }
+
+                }
+
+            }
+            new GenerateOutputExcel().GenerateOutput(dataMap);
+
+        }
+    }
+}
